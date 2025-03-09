4778151236fe7cb04edd8689fc15ac867a9dc518
siddharth020694 <siddharth.thorat@phycominc.com>
2025-03-09 14:27:03 +0530

    Chore : Burger added new line

--- hungry.py
+++ hungry.py
@@ -2,6 +2,7 @@ status = input("Hey , are you Hungry??")
 if status == 'yes':
     print("Let's go n eat samosa")
     print("Eat Pizza")
+    print("Eat Burger")
 else:
     print("Do Your HomeWork")
     
